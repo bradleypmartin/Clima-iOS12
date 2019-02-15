1361743695bd16d27afe226b29e8dd44af7bfe57
bradleypmartin <brad.martin@ibotta.com>
2019-02-14 21:05:56 -0700

    actually updating UI with weather data

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -75,6 +75,8 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
             weatherDataModel.city = json["name"].stringValue
             weatherDataModel.condition = json["weather"][0]["id"].intValue
             weatherDataModel.weatherIconName = weatherDataModel.updateWeatherIcon(condition: weatherDataModel.condition)
+            
+            updateUIWithWeatherData()
         } else {
             cityLabel.text = "Weather Unavailable"
         }
@@ -88,7 +90,11 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     
     //Write the updateUIWithWeatherData method here:
-    
+    func updateUIWithWeatherData() {
+        cityLabel.text = weatherDataModel.city
+        temperatureLabel.text = "\(weatherDataModel.temperature)"
+        weatherIcon.image = UIImage(named: weatherDataModel.weatherIconName)
+    }
     
     
     
